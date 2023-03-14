ed7b49ac13432aa7201c0e67fe946c60f65c17fb
karol <karolwn35@gmail.com>
2023-03-14 18:17:00 +0100

    initial commit

---
+++ run_network.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$USER" != "root" ]
+then
+	echo "Please run this as root or with sudo"
+	exit 1
+fi
+
+if [ -z "$1" ]
+then
+	echo "No argument supplied, use start, stop, delete or status"
+	exit 2
+fi
+
+if [ "$1" == "start" ]
+then
+	# (docker compose -f docker-compose-build.yaml up &) | (timeout --foreground 30 cat; cat >> app.log &)
+	docker compose -f docker-compose-build.yaml up > app.log &
+fi
+
+if [ "$1" == "stop" ]
+then
+        docker compose -f docker-compose-build.yaml stop
+fi
+
+if [ "$1" == "delete" ]
+then
+        docker compose -f docker-compose-build.yaml rm
+fi
+
+if [ "$1" == "status" ]
+then
+        docker ps -a
+fi
+
